3532ce4cfa58c8427e74981ad9e3c2b325ac8540
QuanHertz-pku <aigohz985211@gmail.com>
2024-11-06 19:12:38 +0800

    tyr

--- src/servers/Sender/GetTokenSender.js
+++ src/servers/Sender/GetTokenSender.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem("token");
 const GetTokenSender = (url) => {
   return axios.get(
-    ('http://101.200.13.211:3000/api'+url ),{ headers: {
+    ('/api'+url ),{ headers: {
         Authorization: `Bearer ${token}`
     }}
   ).then(res=>res.data)
--- src/servers/Sender/PutTokenSender.js
+++ src/servers/Sender/PutTokenSender.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem("token");
 const PutTokenSender = (url,data) => {
   return axios.put(
-    ('http://101.200.13.211:3000/api'+url ),data,{ headers: {
+    ('/api'+url ),data,{ headers: {
         Authorization: `Bearer ${token}`
     }}
   ).then(res=>res.data)
